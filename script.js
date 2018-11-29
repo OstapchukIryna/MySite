5b835f9e0667650c987301bca052b01e0c850e7f
OstapchukIryna <blue.hunde@gmail.com>
2018-11-29 04:23:21 +0300

    Add existing file

--- script.js
+++ script.js
@@ -38,3 +38,20 @@ let span = document.getElementsByClassName("modal");
 span.onclick = function () {
 	modal.style.display = "none";
 }
+
+
+let imgs = ["1.png","2.png","3.png","4.png","5.png"];
+let n=0;
+time=1000;
+play=setInterval("chgImg(0)", 7000);
+
+function chgImg(number) {
+if (number!=0) n=number-2;
+ $('#myImg').fadeOut(time, function() {    //для картинок
+  $(this).attr('src', imgs[n]).fadeIn(time);
+ });
+
+
+n++;
+if (n>=imgs.length) n=0;
+}
